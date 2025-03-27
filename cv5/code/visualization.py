883e11ea515baf7bdd8a16115e6d540021852520
rumis <rumisekmt@gmail.com>
2025-03-27 08:14:04 +0100

    cv6

--- cv5/code/visualization.py
+++ cv5/code/visualization.py
@@ -9,6 +9,8 @@ if __name__ == "__main__":
     X_train_n, X_test_n, y_train_n, _ = dataset_std.split_data()
 
     # standardization
+
+
     X_train_standard, _ = dataset_std.scale_data(X_train_s, X_test_s, scale_type='standard')
     dataset_std.plot_all_features_before_after_scaling(X_train_s, X_train_standard, scale_type='Standardization')
     dataset_std.plot_feature_before_after_scaling(X_train_s, X_train_standard, feature_name='mean area')
